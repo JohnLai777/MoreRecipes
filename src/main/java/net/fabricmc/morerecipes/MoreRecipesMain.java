7debbd4d84dcc7fd9d49151620523f60326d938e
John Lai <tommon2345@gmail.com>
2023-06-07 02:45:03 +0800

    Initial commit

---
+++ src/main/java/net/fabricmc/morerecipes/MoreRecipesMain.java
@@ -0,0 +1,9 @@
+package net.fabricmc.morerecipes;
+
+import net.fabricmc.api.ModInitializer;
+public class MoreRecipesMain implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
